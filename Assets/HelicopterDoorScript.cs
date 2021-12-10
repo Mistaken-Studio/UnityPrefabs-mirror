e5b26fb6bb2397b1e1c8de3ca0c52be4ddebec3a
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-12-10 20:02:48 +0100

    Ah yes v2

---
+++ Assets/HelicopterDoorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterDoorScript : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        if(IsOpen != isOpen)
+        {
+            if (IsOpen)
+                OpenDoor();
+            else
+                CloseDoor();
+        }
+    }
+
+    public bool IsOpen;
+    private bool isOpen;
+    public void OpenDoor()
+    {
+        animator.SetBool("IsOpen", true);
+    }
+
+    public void CloseDoor()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
---
+++ Assets/HelicopterScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterScript : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Land()
+    {
+        animator.SetBool("IsLanded", true);
+    }
+
+    public void TakeOff()
+    {
+        animator.SetBool("IsLanded", false);
+    }
+}

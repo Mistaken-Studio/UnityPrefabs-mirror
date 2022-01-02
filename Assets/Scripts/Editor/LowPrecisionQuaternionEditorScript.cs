943ee3c8d88d89f21d5761ddf61fc0ac95c5f535
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-01-02 20:33:28 +0100

    CollorfulEZ

---
+++ Assets/Scripts/Editor/LowPrecisionQuaternionEditorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MeshRenderer))]
+public class LowPrecisionQuaternionEditorScript : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Transform transform = ((MeshRenderer)target).GetComponent<Transform>();
+        if (GUILayout.Button("Round to low precision quaternion"))
+        {
+            Quaternion rotation = transform.localRotation;
+            sbyte _x = (sbyte)(rotation.x * 127f);
+            sbyte _y = (sbyte)(rotation.y * 127f);
+            sbyte _z = (sbyte)(rotation.z * 127f);
+            sbyte _w = (sbyte)(rotation.w * 127f);
+            transform.localRotation = new Quaternion(_x / 127f, _y / 127f, _z / 127f, _w / 127f).normalized;
+        }
+    }
+}

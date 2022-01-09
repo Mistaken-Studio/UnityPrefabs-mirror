08a592bd66a2a536e07c85f631430dcbea5ee585
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-01-09 21:28:17 +0100

    Yes

---
+++ Assets/AnimatorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mistaken.UnityPrefabs
+{
+    public class AnimatorTrigger : MonoBehaviour
+    {
+        public Animator Animator;
+        public string Name = "IsOpen";
+        public bool Toggle = true;
+        public bool Value = true;
+    }
+}
---
+++ Assets/Item.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mistaken.UnityPrefabs
+{
+    public class Item : MonoBehaviour
+    {
+        public ItemType Type = ItemType.None;
+        public ushort Ammo = 0;
+        public bool DestroyAfterUse = false;
+        public bool Locked = false;
+        public bool HasRb = false;
+
+        public enum ItemType
+        {
+            None = -1,
+            KeycardJanitor,
+            KeycardScientist,
+            KeycardResearchCoordinator,
+            KeycardZoneManager,
+            KeycardGuard,
+            KeycardNTFOfficer,
+            KeycardContainmentEngineer,
+            KeycardNTFLieutenant,
+            KeycardNTFCommander,
+            KeycardFacilityManager,
+            KeycardChaosInsurgency,
+            KeycardO5,
+            Radio,
+            GunCOM15,
+            Medkit,
+            Flashlight,
+            MicroHID,
+            SCP500,
+            SCP207,
+            Ammo12gauge,
+            GunE11SR,
+            GunCrossvec,
+            Ammo556x45,
+            GunFSP9,
+            GunLogicer,
+            GrenadeHE,
+            GrenadeFlash,
+            Ammo44cal,
+            Ammo762x39,
+            Ammo9x19,
+            GunCOM18,
+            SCP018,
+            SCP268,
+            Adrenaline,
+            Painkillers,
+            Coin,
+            ArmorLight,
+            ArmorCombat,
+            ArmorHeavy,
+            GunRevolver,
+            GunAK,
+            GunShotgun,
+            SCP330,
+            SCP2176
+        }
+    }
+}

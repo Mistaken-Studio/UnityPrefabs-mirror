22848f19f916fd61819cf0811d73a8a84a31e40f
Barwa <barwa@mistaken.pl>
2022-02-12 14:20:14 +0100

    Poprawa 7 segment display

--- Assets/WorkSpace/Miscellaneous/SegmentDisplay/MutliSegmentDisplayScript.cs
+++ Assets/WorkSpace/Miscellaneous/SegmentDisplay/MutliSegmentDisplayScript.cs
@@ -11,19 +11,25 @@ namespace Mistaken.UnityPrefabs.SegmentDisplay
 
         public MeshRenderer Background;
 
-        public ushort DebugNumber;
+        public string DebugText;
 
-        public ushort CurrentNumber;
-
+        public string CurrentText;
+        void Start()
+        {
+            SetText("");
+        }
         // Update is called once per frame
         void Update()
         {
-            if (CurrentNumber != DebugNumber)
-                SetNumber(DebugNumber);
+            if (CurrentText != DebugText)
+                SetText(DebugText);
         }
-
+        [Obsolete]
         public void SetNumber(ushort num)
         {
+            SetText(num.ToString());
+            return;
+            /*
             if (num >= Mathf.Pow(10, segments.Count))
                 throw new ArgumentOutOfRangeException(nameof(num), $"Segment display can only display up to {Mathf.Pow(10, segments.Count) - 1}");
             if (CurrentNumber == num)
@@ -42,6 +48,17 @@ namespace Mistaken.UnityPrefabs.SegmentDisplay
                 {
                     skip = true;
                 }
+            }
+            */
+        }
+        public void SetText(string text)
+        {
+            if(text.Length > segments.Count)
+                throw new ArgumentOutOfRangeException(nameof(text), $"Segment display can only display up to {segments.Count} letters");
+            text = new String(' ', segments.Count - text.Length) + text;
+            for(int i = 0; i < text.Length; i++)
+            {
+                segments[i].SetChar(text[i]);
             }
         }
     }
--- Assets/WorkSpace/Miscellaneous/SegmentDisplay/SegmentDisplayScript.cs
+++ Assets/WorkSpace/Miscellaneous/SegmentDisplay/SegmentDisplayScript.cs
@@ -17,7 +17,330 @@ namespace Mistaken.UnityPrefabs.SegmentDisplay
         public MeshRenderer LeftBottomSegment;
         public MeshRenderer RightBottomSegment;
         public MeshRenderer BottomSegment;
-
+        static Dictionary<DisplaySegmentChars, bool[]> Chars = new Dictionary<DisplaySegmentChars, bool[]>()
+        {
+            {
+                DisplaySegmentChars.A, new bool[]
+                {
+                    true,
+                    true, true,
+                    true,
+                    true,true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.B, new bool[]
+                {
+                    false,
+                    true, false,
+                    true,
+                    true,true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.C, new bool[]
+                {
+                    true,
+                    true, false,
+                    false,
+                    true,false,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.D, new bool[]
+                {
+                    false,
+                    false, true,
+                    true,
+                    true,true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.E, new bool[]
+                {
+                    true,
+                    true, false,
+                    true,
+                    true,false,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.F, new bool[]
+                {
+                    true,
+                    true, false,
+                    true,
+                    true, false,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.H, new bool[]
+                {
+                    false,
+                    true, true,
+                    true,
+                    true, true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.I, new bool[]
+                {
+                    false,
+                    true, false,
+                    false,
+                    true, false,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.J, new bool[]
+                {
+                    false,
+                    false, true,
+                    false,
+                    true, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.L, new bool[]
+                {
+                    false,
+                    true, false,
+                    false,
+                    true, false,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.N, new bool[]
+                {
+                    false,
+                    false, false,
+                    true,
+                    true, true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.O, new bool[]
+                {
+                    false,
+                    false, false,
+                    true,
+                    true, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.P, new bool[]
+                {
+                    true,
+                    true, true,
+                    true,
+                    true, false,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.Q, new bool[]
+                {
+                    true,
+                    true, true,
+                    true,
+                    false, true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.R, new bool[]
+                {
+                    false,
+                    false, false,
+                    true,
+                    true, false,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.S, new bool[]
+                {
+                    true,
+                    true, false,
+                    true,
+                    false, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.T, new bool[]
+                {
+                    false,
+                    true, false,
+                    true,
+                    true, false,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.U, new bool[]
+                {
+                    false,
+                    true, true,
+                    false,
+                    true, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.Y, new bool[]
+                {
+                    false,
+                    true, true,
+                    true,
+                    false, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.Z, new bool[]
+                {
+                    true,
+                    false, true,
+                    true,
+                    true, false,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.ZERO, new bool[]
+                {
+                    true,
+                    true, true,
+                    false,
+                    true, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.ONE, new bool[]
+                {
+                    false,
+                    false, true,
+                    false,
+                    false, true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.TWO, new bool[]
+                {
+                    true,
+                    false, true,
+                    true,
+                    true, false,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.THREE, new bool[]
+                {
+                    true,
+                    false, true,
+                    true,
+                    false, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.FOUR, new bool[]
+                {
+                    false,
+                    true, true,
+                    true,
+                    false, true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.FIVE, new bool[]
+                {
+                    true,
+                    true, false,
+                    true,
+                    false, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.SIX, new bool[]
+                {
+                    true,
+                    true, false,
+                    true,
+                    true, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.SEVEN, new bool[]
+                {
+                    true,
+                    false, true,
+                    false,
+                    false, true,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.EIGHT, new bool[]
+                {
+                    true,
+                    true, true,
+                    true,
+                    true, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.NINE, new bool[]
+                {
+                    true,
+                    true, true,
+                    true,
+                    false, true,
+                    true
+                }
+            },
+            {
+                DisplaySegmentChars.DASH, new bool[]
+                {
+                    false,
+                    false, false,
+                    true,
+                    false, false,
+                    false
+                }
+            },
+            {
+                DisplaySegmentChars.SPACE, new bool[]
+                {
+                    false,
+                    false, false,
+                    false,
+                    false, false,
+                    false
+                }
+            }
+        };
+        [Obsolete]
         public void SetNumber(byte? num)
         {
             if(num == null)
@@ -129,10 +452,109 @@ namespace Mistaken.UnityPrefabs.SegmentDisplay
                     break;
             }
         }
-
+        public void SetChar(DisplaySegmentChars character)
+        {
+            var colors = Chars[character];
+            SetColor(TopSegment, colors[0] ? EnabledColor : DisabledColor);
+            SetColor(LeftTopSegment, colors[1] ? EnabledColor : DisabledColor);
+            SetColor(RightTopSegment, colors[2] ? EnabledColor : DisabledColor);
+            SetColor(MiddleSegment, colors[3] ? EnabledColor : DisabledColor);
+            SetColor(LeftBottomSegment, colors[4] ? EnabledColor : DisabledColor);
+            SetColor(RightBottomSegment, colors[5] ? EnabledColor : DisabledColor);
+            SetColor(BottomSegment, colors[6] ? EnabledColor : DisabledColor);
+            colors = null;
+        }
+        public bool TrySetChar(char character)
+        {
+            switch (character)
+            {
+                case '0':
+                    SetChar(DisplaySegmentChars.ZERO);
+                    return true;
+                case '1':
+                    SetChar(DisplaySegmentChars.ONE);
+                    return true;
+                case '2':
+                    SetChar(DisplaySegmentChars.TWO);
+                    return true;
+                case '3':
+                    SetChar(DisplaySegmentChars.THREE);
+                    return true;
+                case '4':
+                    SetChar(DisplaySegmentChars.FOUR);
+                    return true;
+                case '5':
+                    SetChar(DisplaySegmentChars.FIVE);
+                    return true;
+                case '6':
+                    SetChar(DisplaySegmentChars.SIX);
+                    return true;
+                case '7':
+                    SetChar(DisplaySegmentChars.SEVEN);
+                    return true;
+                case '8':
+                    SetChar(DisplaySegmentChars.EIGHT);
+                    return true;
+                case '9':
+                    SetChar(DisplaySegmentChars.NINE);
+                    return true;
+                case ' ':
+                    SetChar(DisplaySegmentChars.SPACE);
+                    return true;
+                case '-':
+                    SetChar(DisplaySegmentChars.DASH);
+                    return true;
+            }
+            if(Enum.TryParse<DisplaySegmentChars>(character.ToString(), true, out var chara))
+            {
+                SetChar(chara);
+                return true;
+            }
+            return false;
+        }
+        public void SetChar(char character)
+        {
+            if(!TrySetChar(character))
+                throw new ArgumentOutOfRangeException(nameof(character),"Cannot display this char on 7-segment display");
+        }
         public void SetColor(MeshRenderer obj, Color color)
         {
             obj.material.color = color;
         }
+    }
+    public enum DisplaySegmentChars
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        H,
+        I,
+        J,
+        L,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        Y,
+        Z,
+        ZERO,
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        DASH,
+        SPACE
     }
 }

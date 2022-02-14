03d9cae70862df1a1732b9f1336fb399124a4158
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-02-14 20:56:09 +0100

    Path Lights fun

--- Assets/PathLightController.cs
+++ Assets/PathLightController.cs
@@ -57,37 +57,53 @@ public class PathLightController : MonoBehaviour
         this.StartCoroutine(_setStatus(light, value, force));
     }
 
-    private readonly Dictionary<GameObject, Light> Lights = new Dictionary<GameObject, Light>();
+    private const int FadoutTicks = 15;
+
+    private readonly Dictionary<GameObject, Light[]> Lights = new Dictionary<GameObject, Light[]>();
     private IEnumerator _setStatus(GameObject light, bool value, bool force = false)
     {
         if(!Lights.TryGetValue(light, out var lightC))
         {
-            lightC = light.GetComponentInChildren<Light>();
+            lightC = light.GetComponentsInChildren<Light>();
             Lights[light] = lightC;
         }
 
-        if (value)
-            lightC.enabled = true;
-        else if (force)
-            lightC.enabled = false;
-        else
+        foreach (var item in lightC)
         {
-            var original = 1; // lightC.intensity;
-            while (lightC.intensity > 0)
+            if (value)
+                item.enabled = true;
+            else if (force)
+                item.enabled = false;
+        }
+
+        if (!value && !value)
+        {
+            for (float i = 0; i < 1; i += 1f / FadoutTicks)
             {
-                lightC.intensity -= original * 0.033f;
-                yield return new WaitForSeconds(0.033f);
+                foreach (var item in lightC)
+                    item.intensity -= 1f / FadoutTicks;
+
+                yield return new WaitForSeconds(1f / FadoutTicks);
             }
-            lightC.enabled = false;
-            lightC.intensity = original;
+
+            foreach (var item in lightC)
+            {
+                item.enabled = false;
+                item.intensity = 1;
+            }
         }
     }
 
     public void SetTargetSide(Side targetSide)
     {
-        if (targetSide == Side.CENTER)
+        if (targetSide == Side.NONE)
         {
-            TargetSide = Side.CENTER;
+            TargetSide = Side.NONE;
+            return;
+        } 
+        else if (targetSide == Side.SPECIAL)
+        {
+            TargetSide = SpecialState;
             return;
         }
 
@@ -151,56 +167,57 @@ public class PathLightController : MonoBehaviour
         GameObject[][] minusX = MinusX.ToArray();
         GameObject[][] plusZ = PlusZ.ToArray();
         GameObject[][] minusZ = MinusZ.ToArray();
-        GameObject[][] center = Center.ToArray();
 
         while (true)
         {
-            yield return new WaitForSeconds(.2f);
             GameObject[][] list;
             switch (TargetSide)
             {
-                case Side.CENTER:
-                    Debug.Log("Center :/");
+                case Side.NONE:
+                    yield return new WaitForSeconds(2f);
                     continue;
-                    if (center.Length == 0)
-                        continue;
-                    list = center;
-                    break;
 
                 case Side.PLUS_X:
                     if (plusX.Length == 0)
                         continue;
                     list = plusX;
                     break;
+
                 case Side.MINUS_X:
                     if (minusX.Length == 0)
                         continue;
                     list = minusX;
                     break;
+
                 case Side.PLUS_Z:
                     if (plusZ.Length == 0)
                         continue;
                     list = plusZ;
                     break;
+
                 case Side.MINUS_Z:
                     if (minusZ.Length == 0)
                         continue;
                     list = minusZ;
                     break;
+
                 default:
+                    yield return new WaitForSeconds(2f);
                     continue;
             }
 
-            
-            foreach (var item in list[State - 2 < 0 ? State - 2 + 10 : State - 2])
+
+            foreach (var item in list[State - 2 < 0 ? State - 2 + list.Length : State - 2])
                 SetStatus(item, false);
 
             foreach (var item in list[State])
                 SetStatus(item, true);
 
             State++;
-            if (State > 9)
+            if (State >= list.Length)
                 State = 0;
+
+            yield return new WaitForSeconds(2f / list.Length);
         }
     }
 
@@ -211,190 +228,116 @@ public class PathLightController : MonoBehaviour
         MINUS_X,
         MINUS_Z,
 
+        [System.Obsolete("Use NONE")]
         CENTER = -1,
+        NONE = -1,
+        SPECIAL = -2,
     }
 
     public Side TargetSide;
+    public Side SpecialState = Side.NONE;
     public int State = 0;
 
-    public IEnumerable<GameObject[]> PlusX
+    public IEnumerable<GameObject[]> GenerateList(
+        IEnumerable<GameObject> startList1,
+        IEnumerable<GameObject> startList2,
+        IEnumerable<GameObject> startList3,
+
+        IEnumerable<GameObject> middleList1,
+        IEnumerable<GameObject> middleList2,
+        IEnumerable<GameObject> middleList3,
+
+        IEnumerable<GameObject> endList
+        )
     {
-        get
+        var endListLength = endList.Count();
+
+        if (endListLength == 0)
+            yield break;
+
+        var startList1Length = startList1.Count();
+        var startList2Length = startList2.Count();
+        var startList3Length = startList3.Count();
+
+        var middleList1Length = middleList1.Count();
+        var middleList2Length = middleList2.Count();
+        var middleList3Length = middleList3.Count();
+
+        List<GameObject> tmp = new List<GameObject>();
+
+        for (int i = 0; i < Mathf.Max(Mathf.Max(startList1Length, startList2Length), startList3Length); i++)
         {
-            if (PlusX_Lights.Count == 0)
-                yield break;
-
-            List<GameObject> tmp = new List<GameObject>();
-            for (int i = 0; i < 3; i++)
-            {
-                if (PlusZ_Lights.Count > 0)
-                    tmp.Add(PlusZ_Lights[i]);
-                if (MinusZ_Lights.Count > 0)
-                    tmp.Add(MinusZ_Lights[i]);
-                if (MinusX_Lights.Count > 0)
-                    tmp.Add(MinusX_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                if (PlusX_PlusZ_Lights.Count > 0)
-                    tmp.Add(PlusX_PlusZ_Lights[i]);
-                if (PlusX_MinusZ_Lights.Count > 0)
-                    tmp.Add(PlusX_MinusZ_Lights[i]);
-                if (PlusX_MinusX_Lights.Count > 0)
-                    tmp.Add(PlusX_MinusX_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
-
-            yield return new GameObject[] { PlusX_Lights[2] };
-            yield return new GameObject[] { PlusX_Lights[1] };
-            yield return new GameObject[] { PlusX_Lights[0] };
+            if (startList1Length > i)
+                tmp.Add(startList1.ElementAt(i));
+            if (startList2Length > i)
+                tmp.Add(startList2.ElementAt(i));
+            if (startList3Length > i)
+                tmp.Add(startList3.ElementAt(i));
+            yield return tmp.ToArray();
+            tmp.Clear();
         }
+
+        for (int i = 0; i < Mathf.Max(Mathf.Max(middleList1Length, middleList2Length), middleList3Length); i++)
+        {
+            if (middleList1Length > i)
+                tmp.Add(middleList1.ElementAt(i));
+            if (middleList2Length > i)
+                tmp.Add(middleList2.ElementAt(i));
+            if (middleList3Length > i)
+                tmp.Add(middleList3.ElementAt(i));
+            yield return tmp.ToArray();
+            tmp.Clear();
+        }
+
+        for (int i = endListLength - 1; i >= 0; i--)
+            yield return new GameObject[] { endList.ElementAt(i) };
     }
 
-    public IEnumerable<GameObject[]> MinusX
-    {
-        get
-        {
-            if (MinusX_Lights.Count == 0)
-                yield break;
+    public IEnumerable<GameObject[]> PlusX => GenerateList(
+        PlusZ_Lights,
+        MinusZ_Lights,
+        MinusX_Lights,
 
-            List<GameObject> tmp = new List<GameObject>();
-            for (int i = 0; i < 3; i++)
-            {
-                if (PlusZ_Lights.Count > 0)
-                    tmp.Add(PlusZ_Lights[i]);
-                if (MinusZ_Lights.Count > 0)
-                    tmp.Add(MinusZ_Lights[i]);
-                if (PlusX_Lights.Count > 0)
-                    tmp.Add(PlusX_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
+        PlusX_PlusZ_Lights,
+        PlusX_MinusZ_Lights,
+        PlusX_MinusX_Lights,
 
-            for (int i = 0; i < 4; i++)
-            {
-                if (MinusX_PlusZ_Lights.Count > 0)
-                    tmp.Add(MinusX_PlusZ_Lights[i]);
-                if (MinusX_MinusZ_Lights.Count > 0)
-                    tmp.Add(MinusX_MinusZ_Lights[i]);
-                if (PlusX_MinusX_Lights.Count > 0)
-                    tmp.Add(PlusX_MinusX_Lights[3 - i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
+        PlusX_Lights
+        );
 
-            yield return new GameObject[] { MinusX_Lights[2] };
-            yield return new GameObject[] { MinusX_Lights[1] };
-            yield return new GameObject[] { MinusX_Lights[0] };
-        }
-    }
+    public IEnumerable<GameObject[]> MinusX => GenerateList(
+        PlusZ_Lights,
+        MinusZ_Lights,
+        PlusX_Lights,
 
-    public IEnumerable<GameObject[]> PlusZ
-    {
-        get
-        {
-            if (PlusZ_Lights.Count == 0)
-                yield break;
+        MinusX_PlusZ_Lights,
+        MinusX_MinusZ_Lights,
+        PlusX_MinusX_Lights.Reverse<GameObject>(),
 
-            List<GameObject> tmp = new List<GameObject>();
-            for (int i = 0; i < 3; i++)
-            {
-                if (MinusX_Lights.Count > 0)
-                    tmp.Add(MinusX_Lights[i]);
-                if (MinusZ_Lights.Count > 0)
-                    tmp.Add(MinusZ_Lights[i]);
-                if (PlusX_Lights.Count > 0)
-                    tmp.Add(PlusX_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
+        MinusX_Lights
+        );
 
-            for (int i = 0; i < 4; i++)
-            {
-                if (PlusX_PlusZ_Lights.Count > 0)
-                    tmp.Add(PlusX_PlusZ_Lights[3 - i]);
-                if (MinusX_PlusZ_Lights.Count > 0)
-                    tmp.Add(MinusX_PlusZ_Lights[3 - i]);
-                if (PlusZ_MinusZ_Lights.Count > 0)
-                    tmp.Add(PlusZ_MinusZ_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
+    public IEnumerable<GameObject[]> PlusZ => GenerateList(
+        MinusX_Lights,
+        MinusZ_Lights,
+        PlusX_Lights,
 
-            yield return new GameObject[] { PlusZ_Lights[2] };
-            yield return new GameObject[] { PlusZ_Lights[1] };
-            yield return new GameObject[] { PlusZ_Lights[0] };
-        }
-    }
+        PlusX_PlusZ_Lights.Reverse<GameObject>(),
+        MinusX_PlusZ_Lights.Reverse<GameObject>(),
+        PlusZ_MinusZ_Lights,
 
-    public IEnumerable<GameObject[]> MinusZ
-    {
-        get
-        {
-            if (MinusZ_Lights.Count == 0)
-                yield break;
+        PlusZ_Lights
+        );
 
-            List<GameObject> tmp = new List<GameObject>();
-            for (int i = 0; i < 3; i++)
-            {
-                if (MinusX_Lights.Count > 0)
-                    tmp.Add(MinusX_Lights[i]);
-                if (PlusZ_Lights.Count > 0)
-                    tmp.Add(PlusZ_Lights[i]);
-                if (PlusX_Lights.Count > 0)
-                    tmp.Add(PlusX_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
+    public IEnumerable<GameObject[]> MinusZ => GenerateList(
+        MinusX_Lights,
+        PlusZ_Lights,
+        PlusX_Lights,
 
-            for (int i = 0; i < 4; i++)
-            {
-                if (PlusX_MinusZ_Lights.Count > 0)
-                    tmp.Add(PlusX_MinusZ_Lights[3 - i]);
-                if (MinusX_MinusZ_Lights.Count > 0)
-                    tmp.Add(MinusX_MinusZ_Lights[3 - i]);
-                if (PlusZ_MinusZ_Lights.Count > 0)
-                    tmp.Add(PlusZ_MinusZ_Lights[3 - i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
+        PlusX_MinusZ_Lights.Reverse<GameObject>(),
+        MinusX_MinusZ_Lights.Reverse<GameObject>(),
+        PlusZ_MinusZ_Lights.Reverse<GameObject>(),
 
-            yield return new GameObject[] { MinusZ_Lights[2] };
-            yield return new GameObject[] { MinusZ_Lights[1] };
-            yield return new GameObject[] { MinusZ_Lights[0] };
-        }
-    }
-
-    public IEnumerable<GameObject[]> Center
-    {
-        get
-        {
-            List<GameObject> tmp = new List<GameObject>();
-            for (int i = 0; i < 3; i++)
-            {
-                if (MinusX_Lights.Count > 0)
-                    tmp.Add(MinusX_Lights[i]);
-                if (PlusZ_Lights.Count > 0)
-                    tmp.Add(PlusZ_Lights[i]);
-                if (PlusX_Lights.Count > 0)
-                    tmp.Add(PlusX_Lights[i]);
-                if (MinusZ_Lights.Count > 0)
-                    tmp.Add(MinusZ_Lights[i]);
-                yield return tmp.ToArray();
-                tmp.Clear();
-            }
-
-            yield return new GameObject[0];
-            yield return new GameObject[0];
-            yield return new GameObject[0];
-            yield return new GameObject[0];
-            yield return new GameObject[0];
-            yield return new GameObject[0];
-            yield return new GameObject[0];
-        }
-    }
+        MinusZ_Lights
+        );
 }
